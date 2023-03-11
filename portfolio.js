705c8753e7fd6da2849a39c39164f461946ee9d1
melke22 <107139829+melke22@users.noreply.github.com>
2023-03-12 00:09:43 +0530

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".multiple-text", {
+    strings:["Frontend Developer", 
+    "Spotify Designer",  "Blogger", "YouTuber",],
+    typeSpeed: 95,
+    backSpeed: 60,
+    backDelay: 500,
+})
+
